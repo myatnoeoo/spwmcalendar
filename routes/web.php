a525ad25ae8746fadd83bdd524e3513fcc021588
Myat Noe <52556205+myatnoeoo@users.noreply.github.com>
2020-10-19 20:08:08 +0630

    modal box

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// @ Event Edit
+Route::get('/eventedit', 'EventeditController@edit')->name('event_edit');
